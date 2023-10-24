d96123f312bac507b2b15386437220f7ad8926f7
ahmedBadr1 <feedbackwe4u@gmail.com>
2023-10-25 01:05:23 +0300

    reset login

--- routes/admin.php
+++ routes/admin.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix'=>'admin','as'=>'admin.'],function (){
-    Route::get('login',[\App\Http\Controllers\Admin\Auth\LoginController::class,'showLoginForm'])->name('showLogin');
-    Route::post('login/check',[\App\Http\Controllers\Admin\Auth\LoginController::class,'login'])->name('login');
+    Route::get('login',[\App\Http\Controllers\Admin\Auth\LoginController::class,'showLoginForm'])->name('login');
+    Route::post('login',[\App\Http\Controllers\Admin\Auth\LoginController::class,'login']);
     Route::get('register',[\App\Http\Controllers\Admin\Auth\LoginController::class,'register'])->name('register');
 
     Route::group(['middleware'=>'auth'],function (){
